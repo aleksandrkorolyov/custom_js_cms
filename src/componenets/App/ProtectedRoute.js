c987171539bf7ca1e30f6b5c3510dfdf74cfd0c7
Aliaksandr <aliaksandr.karaliou@realvnc.com>
2023-02-17 15:22:20 +0100

    Added auto-logout during 10 min of inavtivity

--- src/componenets/App/ProtectedRoute.js
+++ src/componenets/App/ProtectedRoute.js
@@ -1,6 +1,24 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { useIdle } from "react-use";
+import useToken from "./useToken";
 
-const ProtectedRoute = ({token}) => {
+const ProtectedRoute = () => {
+
+    const { token, removeToken, setToken } = useToken();
+
+    const navigate = useNavigate();
+
+    //Set idle timeout for 5 minutes
+    const isIdle = useIdle(300000);
+    
+    useEffect( () => {
+        if(isIdle === true) {
+            removeToken();
+            navigate(0)
+        }
+    }, [isIdle] );
+
     return token ? <Outlet/> : <Navigate to={'/login'} replace />;
 }
 
